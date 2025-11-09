005ef55545f9774a31fc16792d8aff4ac20410f2
pheralb <pablohdez4340@gmail.com>
2025-11-09 18:33:06 +0000

    feat: update ESLint config to include gitignore and prettier plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,20 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 // Plugins:
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
 import nextVitals from "eslint-config-next/core-web-vitals";
 
+// Gitignore:
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
 const eslintConfig = defineConfig([
+  includeIgnoreFile(gitignorePath),
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
   ...nextVitals,
   ...nextTs,
+  prettier,
 ]);
 
 export default eslintConfig;
