cb766c90ffa5bfc415e2f82e985bc1b6b6d526e1
pheralb <pablohdez4340@gmail.com>
2025-12-04 17:32:32 +0000

    feat: update Article component to enhance styling and structure

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -2,7 +2,16 @@ import { cn } from "@/utils/cn";
 import type { ComponentProps } from "react";
 
 const Article = ({ children }: ComponentProps<"article">) => {
-  return <article className={cn("markdown")}>{children}</article>;
+  return (
+    <article
+      className={cn(
+        "prose prose-zinc dark:prose-invert",
+        "w-full max-w-full text-pretty",
+      )}
+    >
+      {children}
+    </article>
+  );
 };
 
 export default Article;
