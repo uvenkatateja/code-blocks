82a0665378a29f848b7c56b8cde05390cb7fff0e
pheralb <pablohdez4340@gmail.com>
2025-12-13 14:46:11 +0000

    feat: add ``className`` prop to Article component for improved styling flexibility

--- src/components/docs/doc-article.tsx
+++ src/components/docs/doc-article.tsx
@@ -1,13 +1,14 @@
 import { cn } from "@/utils/cn";
 import type { ComponentProps } from "react";
 
-const Article = ({ children }: ComponentProps<"article">) => {
+const Article = ({ children, className }: ComponentProps<"article">) => {
   return (
     <article
       className={cn(
         "prose prose-neutral dark:prose-invert",
         "prose-h2:font-headings",
         "w-full max-w-full text-pretty",
+        className,
       )}
     >
       {children}
