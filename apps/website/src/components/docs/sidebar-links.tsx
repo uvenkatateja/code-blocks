bfa46ecfb8b99a6dde98a8031c38f492b36581f4
pheralb <pablohdez4340@gmail.com>
2025-12-28 19:29:43 +0000

    fix: add optional icon and href properties in SidebarLinks component

--- apps/website/src/components/docs/sidebar-links.tsx
+++ apps/website/src/components/docs/sidebar-links.tsx
@@ -19,12 +19,12 @@ const SidebarLinks = () => {
                 <SidebarSubItem
                   key={link.title}
                   title={link.title}
-                  icon={link.icon!}
+                  icon={link.icon}
                 >
                   {link.subItems.map((subItem) => (
                     <SidebarLinkItem
                       key={subItem.href}
-                      href={subItem.href!}
+                      href={subItem.href ?? ""}
                       isActive={pathname === subItem.href}
                     >
                       <p className="truncate">{subItem.title}</p>
@@ -36,7 +36,7 @@ const SidebarLinks = () => {
             return (
               <SidebarLinkItem
                 key={link.href}
-                href={link.href!}
+                href={link.href ?? ""}
                 isActive={pathname === link.href}
               >
                 {link.icon && <link.icon size={16} className="shrink-0" />}
