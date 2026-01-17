703cdadc91cf3414097753658a2515dd87f09c79
pheralb <pablohdez4340@gmail.com>
2026-01-17 20:10:21 +0000

    fix: improve sugar-high code-block-client

--- apps/website/src/components/previews/code-block-sugar-high-example.tsx
+++ apps/website/src/components/previews/code-block-sugar-high-example.tsx
@@ -5,16 +5,17 @@ import {
 } from "@/components/code-block/code-block";
 
 import { CodeBlockSugarHigh } from "@/components/code-block/client/sugar-high";
+import { CopyButton } from "@/components/code-block/copy-button";
 
 const CodeBlockSugarHighExample = () => {
+  const code = `console.log('Hello, world!');`;
   return (
     <CodeBlock>
-      <CodeBlockHeader
-        language="ts"
-        title="Code block + Sugar High"
-      />
+      <CodeBlockHeader language="ts" title="Code block + Sugar High">
+        <CopyButton content={code} />
+      </CodeBlockHeader>
       <CodeBlockContent>
-        <CodeBlockSugarHigh code="console.log('Hello, world!');" />
+        <CodeBlockSugarHigh code={code} />
       </CodeBlockContent>
     </CodeBlock>
   );
