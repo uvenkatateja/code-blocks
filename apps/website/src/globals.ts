dadb00dd74a9385bcc4876993420862f02b8dd03
pheralb <pablohdez4340@gmail.com>
2026-01-28 12:16:52 +0000

    fix: improve metadata

--- apps/website/src/globals.ts
+++ apps/website/src/globals.ts
@@ -1,5 +1,5 @@
 export const globals = {
-  title: 'pheralb/codeblocks',
+  title: 'Code Blocks',
   subTitle: 'A code-block UI component and utilities',
   githubUrl: "https://github.com/pheralb/code-blocks",
   twitterUrl: "https://twitter.com/pheralb_",
