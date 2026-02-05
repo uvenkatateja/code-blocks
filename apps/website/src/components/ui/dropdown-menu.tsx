9ce11342622167e21eea941c359b34c48d67e359
pheralb <pablohdez4340@gmail.com>
2026-02-05 10:55:37 +0000

    fix: fixed DropdownMenuContent width size

--- apps/website/src/components/ui/dropdown-menu.tsx
+++ apps/website/src/components/ui/dropdown-menu.tsx
@@ -39,7 +39,7 @@ function DropdownMenuContent({
         <MenuPrimitive.Popup
           data-slot="dropdown-menu-content"
           className={cn(
-            "data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 data-closed:zoom-out-95 data-open:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[side=inline-start]:slide-in-from-right-2 data-[side=inline-end]:slide-in-from-left-2 z-50 max-h-(--available-height) w-(--anchor-width) min-w-32 origin-(--transform-origin) overflow-x-hidden overflow-y-auto rounded-md bg-white p-1 text-neutral-900 shadow-md ring-1 ring-neutral-900/10 duration-100 outline-none data-closed:overflow-hidden dark:bg-neutral-900 dark:text-neutral-50 dark:ring-neutral-50/10",
+            "data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 data-closed:zoom-out-95 data-open:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[side=inline-start]:slide-in-from-right-2 data-[side=inline-end]:slide-in-from-left-2 z-50 max-h-(--available-height) min-w-32 origin-(--transform-origin) overflow-x-hidden overflow-y-auto rounded-md bg-white p-1 text-neutral-900 shadow-md ring-1 ring-neutral-900/10 duration-100 outline-none data-closed:overflow-hidden dark:bg-neutral-900 dark:text-neutral-50 dark:ring-neutral-50/10",
             className,
           )}
           {...props}
