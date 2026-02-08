f8b01df3f472dfaa51ab4841888517cda4350270
pheralb <pablohdez4340@gmail.com>
2026-02-08 13:17:02 +0000

    fix: remove text size from TabsContent className

--- apps/website/src/components/ui/tabs.tsx
+++ apps/website/src/components/ui/tabs.tsx
@@ -75,7 +75,7 @@ function TabsContent({ className, ...props }: TabsPrimitive.Panel.Props) {
   return (
     <TabsPrimitive.Panel
       data-slot="tabs-content"
-      className={cn("flex-1 text-sm outline-none", className)}
+      className={cn("flex-1 outline-none", className)}
       {...props}
     />
   );
